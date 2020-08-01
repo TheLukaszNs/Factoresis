3abbae513b07d77fac6c1f52edf7743beb779180
TheLukaszNs <lmysliwiec10@gmail.com>
2020-08-01 15:25:19 +0200

    Grid Preview

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private int CellSize = 1;
+
+    public Vector3 WorldToGrid(Vector3 position)
+    {
+        return new Vector3(
+            Mathf.RoundToInt(position.x / CellSize) * CellSize,
+            0f,
+            Mathf.RoundToInt(position.z / CellSize) * CellSize
+        );
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log(WorldToGrid(hit.point));
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int x = CellSize; x < 15 * CellSize; x += CellSize)
+        {
+            for (int y = 0; y < 15 * CellSize - CellSize; y += CellSize)
+            {
+                Gizmos.DrawLine(WorldToGrid(new Vector3(x - CellSize, 0f, y)), WorldToGrid(new Vector3(x, 0f, y)));
+                Gizmos.DrawLine(WorldToGrid(new Vector3(x, 0f, y)), WorldToGrid(new Vector3(x, 0f, y + CellSize)));
+            }
+        }
+    }
+
+}
