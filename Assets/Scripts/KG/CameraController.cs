b87fbaa0f99f4831c62ccc0b13c3778b528a5d87
Daniel <dagohar246@gmail.com>
2020-08-09 19:39:19 +0200

    Camera Controller Fix

--- Assets/Scripts/KG/CameraController.cs
+++ Assets/Scripts/KG/CameraController.cs
@@ -6,10 +6,8 @@ public class CameraController : MonoBehaviour
 
     [Range(1.0f, 1000.0f)]
     public float mouseSensitivity = 100.0f;  // Select sensitivity
-
-    private float mainCameraXRotation, mainCameraYRotation;
-
-    private const float defaultMainCameraXRotation = 45.0f, defaultMainCameraYRotation = 0.0f; // value of Main Camera X and Y Rotation on Start
+    private float mainCameraXRotation;
+    private const float defaultMainCameraXRotation = 45.0f; // value of Main Camera X Rotation on Start
     private const float cameraSpeed = 100.0f;    // Camera Empty Object const speed
     private const float margin = 10.0f;
     private const float zoom = 5.0f;
@@ -17,17 +15,7 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         mainCamera.transform.rotation = Quaternion.Euler(45.0f, 0.0f, 0.0f); // Select Main Camera X Rotation on Start
-
         mainCameraXRotation = defaultMainCameraXRotation;
-        mainCameraYRotation = defaultMainCameraYRotation;
-    }
-
-    void LateUpdate()
-    {
-        Vector3 CameraPos = mainCamera.transform.position;
-
-        // Clamp camera position
-        mainCamera.transform.position = new Vector3(Mathf.Clamp(CameraPos.x, -200f, 200f), Mathf.Clamp(CameraPos.y, 1f, 200f), Mathf.Clamp(CameraPos.z, -200f, 200f));
     }
 
     void Update()
@@ -39,14 +27,23 @@ public class CameraController : MonoBehaviour
         if (Input.GetMouseButton(1))
         {
             transform.Rotate(Vector3.down * mouseX);    // Rotate Camera Empty Object
-
-            mainCameraXRotation -= mouseY;
+            mainCameraXRotation += mouseY;
             mainCameraXRotation = Mathf.Clamp(mainCameraXRotation, 0.0f, 90.0f);    // Rotation between 0 and 90 angle
-
-            mainCameraYRotation += mouseX;
-
-            mainCamera.transform.localRotation = Quaternion.Euler(mainCameraXRotation, mainCameraYRotation, 0.0f); // Rotate Main Camera
+            mainCamera.transform.localRotation = Quaternion.Euler(mainCameraXRotation, 0.0f, 0.0f); // Rotate Main Camera
         }
+
+        // Zoom in
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0.0f && mainCamera.fieldOfView > 30.0f)
+        {
+            mainCamera.fieldOfView -= zoom;
+        }
+
+        // Zoom out
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0.0f && mainCamera.fieldOfView < 60.0f)
+        {
+            mainCamera.fieldOfView += zoom;
+        }
+
         else
         {
             // Movement
@@ -54,33 +51,18 @@ public class CameraController : MonoBehaviour
             {
                 transform.Translate(Vector3.forward * cameraSpeed * Time.deltaTime);    // Forward
             }
-
             if (Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x < margin)
             {
                 transform.Translate(Vector3.left * cameraSpeed * Time.deltaTime);   // Left
             }
-
             if (Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y < margin)
             {
                 transform.Translate(Vector3.back * cameraSpeed * Time.deltaTime);   // Backward
             }
-
             if (Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x > Screen.width - margin)
             {
                 transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);  // Right
             }
-        }
-
-        // Zoom in
-        if (Input.GetAxis("Mouse ScrollWheel") > 0.0f && mainCamera.fieldOfView > 30.0f)
-        {
-            mainCamera.fieldOfView -= zoom;
-        }
-
-        // Zoom out
-        if (Input.GetAxis("Mouse ScrollWheel") < 0.0f && mainCamera.fieldOfView < 60.0f)
-        {
-            mainCamera.fieldOfView += zoom;
         }
     }
 }
