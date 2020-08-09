bedce1c9528db257fc1f22c22f83bf4292bdb7b1
TheLukaszNs <lmysliwiec10@gmail.com>
2020-08-09 13:34:54 +0200

    Add Forest Generator
    Added forest generation preview

---
+++ Assets/Scripts/ŁM/ForestGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestGenerator : MonoBehaviour
+{
+    public List<TreeType> trees = new List<TreeType>();
+
+    private void Start()
+    {
+        for (float x = 0f; x < 100; x += 5f)
+        {
+            for (float z = 0f; z < 100; z += 5f)
+            {
+                foreach (TreeType tree in trees)
+                {
+                    if (Random.value <= tree.spawnRate)
+                    {
+                        Instantiate(tree.treePrefab, new Vector3(x, 0f, z), Quaternion.Euler(Vector3.up));
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class TreeType
+{
+    public string name;
+    public GameObject treePrefab;
+    public float spawnRate = .1f;
+}
